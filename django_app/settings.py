e7d1da0e8da25b596430b4b7bc320f001dc052aa
TanishB567 <bansaltanish05@gmail.com>
2024-09-25 17:00:49 +0100

    changed settings.py to be able to deploy to vercel

--- django_app/settings.py
+++ django_app/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','frontend')
+STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'frontend/build/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'frontend/build/static')
-]
